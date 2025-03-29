15fe811cba1ac54075ced31283df8ac84a45cb24
Ruslan Romanenko <71728377+r-romanenko@users.noreply.github.com>
2025-03-28 17:06:42 -0700

    trying to add green color to overlaps with previous iteration

--- letterPronunciation.py
+++ letterPronunciation.py
@@ -29,6 +29,17 @@ letters = {
     'z': 'zee'
 }
 
+class bcolors:
+    BLACK = '\u001b[30m'
+    RED = '\u001b[31m'
+    GREEN = '\u001b[32m'
+    YELLOW = '\u001b[33m'
+    BLUE = '\u001b[34m'
+    MAGENTA = '\u001b[35m'
+    CYAN = '\u001b[36m'
+    WHITE = '\u001b[37m'
+    RESET = '\u001b[0m'
+
 # go through the letters in the dictionary.
 # for each letter, make a new string and add it to a list (later)
 # get the pronunciation, and then go through that pronunciation one more time
@@ -44,9 +55,41 @@ for key in letters.keys():
         newWord = ""
         for letter in curWord:
             newWord += letters[letter]
+
+        # go through the newWord, seeing how much the curWord overlaps
+        journey += f", {bcolors.GREEN}"
+
+        letter_added = False
+
+        index = len(curWord)
+        for j in range(len(curWord)):
+            if curWord[j] != newWord[i]:
+                # if i == 0:
+                #     journey += newWord[0]
+                index = j + 1
+                break
+            else:
+                journey += newWord[j]
+                letter_added = True
+        
+        journey += bcolors.RESET
+        if not letter_added:
+            journey += newWord[0]
+        # index += 1
+
+        while index < len(newWord):
+            journey += newWord[index]
+            index += 1
+        
+
         curWord = newWord
-        journey += f", {curWord}"
+        # journey += f", {curWord}"
     pronunciations.append(journey)
     
 for journ in pronunciations:
     print(journ)
+
+
+
+
+print()
